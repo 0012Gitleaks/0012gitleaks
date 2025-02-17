d45a37ae4f05fe92337fc41dd4ec6409715f78d3
Sugandasree <107237480+Sugayuv1@users.noreply.github.com>
2025-02-17 19:10:48 +0530

    Update api.go

--- testdata/repos/staged/api/api.go
+++ testdata/repos/staged/api/api.go
@@ -6,6 +6,7 @@ func PrintHello() {
 
 	aws_token2 := "AKIALALEMEL33243OLIA" // this one is not
 	aws_token2 := "AKIALALEMEL33243OLIA" // this one is not
+	aws_token2 := "AKIALALEMEL33243OLIA" // this one is not
 	fmt.Println(aws_token)
 	fmt.Println(aws_token2)
 }
